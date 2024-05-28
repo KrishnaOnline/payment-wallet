c1094967b271a3f49841fd8b4b39348e40fb561c
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-28 20:00:22 +0530

    fix CORS

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const mainRouter = require("./routes/index");
 const cors = require("cors");
 require("dotenv").config();
 
 // app.use(cors());
 app.use(express.json());
-
 app.use(
     cors({
-        origin: ["https://payment-wallet-kv.vercel.app"],
+        origin: "https://payment-wallet-kv.vercel.app",
         credentials: true,
     })
 );
 
+app.use("/api", mainRouter);
+
 dbConnect();
 app.get("/", (req, res) => {
-    res.json(`Server is Up and Running...`);
+    return res.json({
+        success: true,
+        message: "Server is Up and Running..."
+    });
 })
-
-app.use("/api", mainRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is Running at ${PORT}`);
